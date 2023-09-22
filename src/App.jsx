a274be7efd201b8a4996d1e2cc12a72303630df5
Leo9428 <127988282+Leo9428@users.noreply.github.com>
2023-09-22 15:42:31 -0300

    segundo commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ClientSession } from "mongodb";
+import { useState } from "react";
+
+
+export const App = () => {
+const initialState = 0;
+const  [CountQueuingStrategy,setCounter] = useState(initialState)
+
+
+
+}
